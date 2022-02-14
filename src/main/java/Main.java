dad7a4138d3ec11e04b55bfd94e5036d13a842d5
ScharrerRaphael <scharrerraphael44@gmail.com>
2022-02-14 22:08:07 +0100

    conflict test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
         System.out.println("Arbeiter1");
         System.out.println("Arbeiter2");
+        System.out.println("Arbeiter2 arbeiter weiter dahin, trotz Abänderungen im remote Repo");
         System.out.println("Arbeiter1 fügt etwas hinzu worauf Arbeiter2 pullt");
     }
 }
